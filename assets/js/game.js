e6fb2a8dbbb8f39360cdf4adb5b148ce7c8395fa
Sarah Gambles <sarahgambles14@gmail.com>
2020-08-09 17:49:05 -0600

    added java

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,22 @@
-window.alert("This is an alert! JavaScript is running!");
+// this creates a function named "fight"
+var playerName = window.prompt("What is your robot's name?");
+//  Note the lack of quotations around playerName
+console.log(playerName);
+
+var playerName = "Tony the Robot";
+
+// "Your robot, Tony the Robot, has won!"
+console.log("Your robot, " + playerName + ", has won!");
+
+console.log("This logs a string, good for leaving yourself a message");
+// this will do math and log 20
+console.log(10 + 10);
+//what is this?
+console.log("Our robot's name is " + playerName);
+function fight() 
+
+{
+    window.alert("The fight has begun!");
+}
+
+// fight();
